ed2d9ad3f7601064f3a4d5f9db3b3b4a4750bafb
craigB0T <Craig@Requires-Adjustment-2.local>
2016-06-01 21:33:45 -0400

    Kittens vs. Zombies
    Initial branch for Plants vs Zombies version.

---
+++ Unity Project/Tower Defense/Assets/Scripts/MoveDestination.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MoveDestination : MonoBehaviour
+{
+
+	public Transform goal;
+
+	void Start ()
+	{
+		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent.destination = goal.position; 
+	}
+}
